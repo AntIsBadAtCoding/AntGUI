02fcf9e97d92ef48b01ae073913b8d912ca61874
AntIsBadAtCoding <anthonyaldrete1@gmail.com>
2025-01-25 18:07:58 -0800

    Update Source.lua

--- Source.lua
+++ Source.lua
@@ -3,9 +3,9 @@ local library = {}
 local local_player = game:GetService("Players").LocalPlayer
 local mouse = local_player:GetMouse()
 local uis = game:GetService("UserInputService")
+
 local keybind
 local state = true
-local tabs = {}
 
 function library:Create(obj, properties, parent)
 	local object = Instance.new(obj)
@@ -52,22 +52,23 @@ function library:New(Name)
 	local ui = library:Create("Frame", {
 		Size = UDim2.new(0, 500, 0, 400),
 		BackgroundColor3 = Color3.fromRGB(200, 100, 100),
-		Position = UDim2.new(0, 50, 0, 100)
+		Position = UDim2.new(0, 50, 0, 100),
+		Draggable = true
 	}, screengui)
 	
 	local topbar = library:Create("Frame", {
 		Size = UDim2.new(0, 500, 0, 30),
-		BackgroundColor3 = Color3.fromRGB(150, 50, 50),
+		BackgroundColor3 = Color3.fromRGB(175, 75, 75),
 		Position = UDim2.new(0, 0, 0, 0)
 	}, ui)
 	
 	local sidebar = library:Create("Frame", {
 		Size = UDim2.new(0, 70, 0, 370),
-		BackgroundColor3 = Color3.fromRGB(135, 35, 35),
+		BackgroundColor3 = Color3.fromRGB(150, 50, 50),
 		Position = UDim2.new(0, 0, 0, 30)
 	}, ui)
 	
-	local Ttile = library:Create("TextLabel", {
+	local Title = library:Create("TextLabel", {
 		Size = UDim2.new(0, 500, 0, 30),
 		BackgroundTransparency = 1,
 		Position = UDim2.new(0, 0, 0, 0),
@@ -84,15 +85,15 @@ function library:New(Name)
 	--// tabs \\--
 	
 	local selectedtab = 1
-	local tabs = 0
-	local tablist = {}
+	local tabcount = 0
+	local tabs = {}
 	
-	function library.MakeTab(image, color, currenttab)
-		tabs += 1
-		local posxoffset = ((tabs * 70) - 65)
+	function tabs.MakeTab(image, currenttab)
+		tabcount += 1
+		local posxoffset = ((tabcount * 70) - 65)
 		local tab = library:Create("ImageButton", {
 			Image = image,
-			BackgroundColor3 = color,
+			BackgroundColor3 = Color3.fromRGB(200, 100, 100),
 			BorderColor3 = Color3.fromRGB(0, 0, 0),
 			Size = UDim2.fromOffset(60, 60),
 			Position = UDim2.fromOffset(5, posxoffset)
@@ -103,17 +104,23 @@ function library:New(Name)
 		end)
 	end
 	
+	function tabs.FixButtons(Tab)
+		selectedtab = Tab
+		tabevent:Fire(Tab)
+	end
+	
 	--// Make a button \\--
 	
-	function library.MakeButton(name, color, tab, buttoncount, func)
+	function tabs.MakeButton(name, tab, buttoncount, func)
 		local Ypos = ((buttoncount * 55) -50)
 		local button = library:Create("TextButton", {
 			Text = name,
 			TextScaled = true,
-			BackgroundColor3 = color,
+			BackgroundColor3 = Color3.fromRGB(200, 100, 100),
 			Size = UDim2.fromOffset(387, 50),
 			Position = UDim2.fromOffset(5, Ypos)
 		}, buttonbar)
+		
 		button.MouseButton1Down:Connect(func)
 		
 		tabevent.Event:Connect(function()
@@ -125,14 +132,14 @@ function library:New(Name)
 		end)
 	end
 	
-	function library.MakeToggle(name, color, tab, count, func)
+	function tabs.MakeToggle(name, tab, count, func)
 		local toggled = false
 		local Ypos = ((count * 55) -50)
 		
 		local button = library:Create("TextButton", {
 			Text = name,
 			TextScaled = true,
-			BackgroundColor3 = color,
+			BackgroundColor3 = Color3.fromRGB(200, 100, 100),
 			Size = UDim2.fromOffset(330, 50),
 			Position = UDim2.fromOffset(5, Ypos)
 		}, buttonbar)
@@ -144,16 +151,20 @@ function library:New(Name)
 			BackgroundColor3 = Color3.fromRGB(200, 0, 0)
 		}, buttonbar)
 		
-		local toggledevent = Instance.new("BindableEvent", Circle)
-		toggledevent.Name = "bind"
+		local toggled_event = Instance.new("BindableEvent", Circle)
+		toggled_event.Name = "event"
 		
 		local corner = library:Create("UICorner", {
 			CornerRadius = UDim.new(1, 0)
 		}, Circle)
 		
-		button.MouseButton1Down:Connect(func)
+		button.MouseButton1Down:Connect(function()
+			toggled = not toggled
+			toggled_event:Fire(toggled)
+		end)
+		Circle.event.Event:Connect(func) --player func
 		
-		Circle.bind.Event:connect(function(Data)
+		Circle.event.Event:connect(function(Data)
 			if Data == true then
 				Circle.BackgroundColor3 = Color3.fromRGB(0, 200, 0)
 			else
@@ -171,6 +182,86 @@ function library:New(Name)
 			end
 		end)
 	end
-end
+	
+	function tabs.MakeSlider(name, min, max, tab, count, func)
+		local Ypos = ((count * 55) -50)
 
+		local sliderbase = library:Create("Frame", {
+			BackgroundColor3 = Color3.fromRGB(200, 100, 100),
+			Size = UDim2.fromOffset(190, 50),
+			Position = UDim2.fromOffset(5, Ypos)
+		}, buttonbar)
+		
+		local Textbox = library:Create("TextLabel", {
+			Text = name,
+			TextScaled = true,
+			BackgroundColor3 = Color3.fromRGB(200, 100, 100),
+			Size = UDim2.new(1, 0, 1, 0),
+			Position = UDim2.fromOffset(0, 0)
+		}, sliderbase)
+		
+		local bar = library:Create("Frame", {
+			BackgroundColor3 = Color3.fromRGB(175, 75, 75),
+			Size = UDim2.fromOffset(192, 20),
+			Position = UDim2.fromOffset(291, 25),
+			AnchorPoint = Vector2.new(0.5, 0.5)
+		}, sliderbase)
+		
+		local slider = library:Create("ImageButton", {
+			BackgroundColor3 = Color3.fromRGB(200, 100, 100),
+			Size = UDim2.fromOffset(25, 40),
+			Position = UDim2.fromOffset(268, 5)
+		}, sliderbase)
+		
+		local move_event = Instance.new("BindableEvent", slider)
+		move_event.Name = "event"
+		
+		local corner = library:Create("UICorner", {
+			CornerRadius = UDim.new(1, 0)
+		}, bar)
+		
+		slider.MouseButton1Down:Connect(function()
+			local dragging = true
+			
+			local function move(pos, value)
+				local newpos = math.clamp(pos-150, 195, 363)
+				slider.Position = UDim2.fromOffset(newpos, 5)
+				
+				Textbox.Text = name .. ": " .. value
+			end
+			
+			local moveconnection
+			moveconnection = uis.InputChanged:Connect(function(input)
+				if input.UserInputType == Enum.UserInputType.MouseMovement then
+					local SizeScale = math.clamp((input.Position.X - bar.AbsolutePosition.X) / bar.AbsoluteSize.X, 0, 1)
+					local value = math.floor(min + ((max - min) * SizeScale))
+					move(input.Position.X, value)
+					slider.event:Fire(value)
+				end
+			end)
+			
+			local releaseconection
+			releaseconection = uis.InputEnded:Connect(function(input)
+				if input.UserInputType == Enum.UserInputType.MouseButton1 then
+					moveconnection:Disconnect()
+					releaseconection:Disconnect()
+					dragging = false
+				end
+			end)
+		end)
+		
+		slider.event.Event:Connect(func)
+		
+		tabevent.Event:Connect(function()
+			if selectedtab == tab then
+				sliderbase.Visible = true
+				bar.Visible = true
+			else
+				sliderbase.Visible = false
+				bar.Visible = false
+			end
+		end)
+	end
+	return tabs
+end
 return library
