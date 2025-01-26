72d33a28960d1a96260eea45ae99e48c86520bde
AntIsBadAtCoding <anthonyaldrete1@gmail.com>
2025-01-25 18:16:21 -0800

    Update Example.lua

--- Example.lua
+++ Example.lua
@@ -1,17 +1,30 @@
-local lib = loadstring(game:HttpGet('https://raw.githubusercontent.com/AntIsBadAtCoding/AntGUI/refs/heads/main/Source.lua'))
+local lib = loadstring(game:HttpGet('https://raw.githubusercontent.com/AntIsBadAtCoding/AntGUI/refs/heads/main/Source.lua')) --loads the source
 
-lib:New("Ants UI lib")
+local window = lib.New("Ants UI lib") -- makes the window
 
-lib.SetKeybind(Enum.KeyCode.RightAlt)
+lib.SetKeybind(Enum.KeyCode.RightAlt) -- sets the keybind to open and close the ui
 
-lib.MakeTab("rbxassetid://7205257578", Color3.fromRGB(200, 100, 100), 1)
+local tab = window.MakeTab("rbxassetid://7205257578", 1) -- makes a tab
 
-lib.MakeTab("rbxassetid://7205257578", Color3.fromRGB(200, 100, 100), 2)
+local tab2 = window.MakeTab("rbxassetid://7205257578", 2)
 
-lib.MakeButton("Button", Color3.fromRGB(200, 100, 100), 1, 1, function()
-	print("test")
+-- makebutton("name", tab, ui placement, function)
+local button1 = window.MakeButton("button1", 1, 1, function() -- makes a button
+	print("button pressed")
 end)
 
-lib.MakeToggle("Toggle", Color3.fromRGB(200, 100, 100), 1, 2, function()
-	
+local button2 = window.MakeButton("button2", 2, 1, function()
+	print("button2 pressed")
 end)
+
+-- maketoggle("name", tab, ui placement, func) val will always be true or false
+local toggle1 = window.MakeToggle("Toggle", 1, 2, function(val) -- makes a toggle
+	print(val)
+end)
+
+-- makeslider("slidername", min, max, tab, ui placement, function) vall will be somewhere in the range you set
+local slider1 = window.MakeSlider("Slider name", 0, 50, 1, 3, function(val) -- makes a slider
+	print(val)
+end)
+
+window.FixButtons(1)
